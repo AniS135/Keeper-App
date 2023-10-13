d7fa1cc3839aec3c4c7cd85ff628a031be721f0e
Neeraj Kumar <nk7075034@gmail.com>
2023-10-13 18:05:55 +0530

    feat: implemented the login and sigup functionality

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,48 +3,44 @@ import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
-import LoginWrapper from "./Login";
-
 
 function App() {
-  const [notes, setNotes] = useState([]);
+    const [notes, setNotes] = useState([]);
 
-  function addNote(newNote) {
-    setNotes(prevNotes => {
-      return [...prevNotes, newNote];
-    });
-  }
+    function addNote(newNote) {
+        setNotes((prevNotes) => {
+            return [...prevNotes, newNote];
+        });
+    }
 
-  function deleteNote(id) {
-    setNotes(prevNotes => {
-      return prevNotes.filter((noteItem, index) => {
-        return index !== id;
-      });
-    });
-  }
+    function deleteNote(id) {
+        setNotes((prevNotes) => {
+            return prevNotes.filter((noteItem, index) => {
+                return index !== id;
+            });
+        });
+    }
 
-  return (
-
-    <div className="main-container">
-      <LoginWrapper />
-      <Header />
-      <CreateArea onAdd={addNote} />
-      <div className="NotesContainer">
-        {notes.map((noteItem, index) => {
-          return (
-            <Note
-              key={index}
-              id={index}
-              title={noteItem.title}
-              content={noteItem.content}
-              onDelete={deleteNote}
-            />
-          );
-        })}
-      </div>
-      <Footer />
-    </div>
-  );
+    return (
+        <div className="main-container">
+            <Header />
+            <CreateArea onAdd={addNote} />
+            <div className="NotesContainer">
+                {notes.map((noteItem, index) => {
+                    return (
+                        <Note
+                            key={index}
+                            id={index}
+                            title={noteItem.title}
+                            content={noteItem.content}
+                            onDelete={deleteNote}
+                        />
+                    );
+                })}
+            </div>
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
